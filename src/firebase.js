d35c48d9fb29293abb41340f8f7d428d95cd1c5f
Arsh <kohliarsh9@gmail.com>
2021-06-22 17:54:28 +0530

    finished touches

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import firebaseConfig from "./firebaseKeys";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC7AGFM7jtG-SOxGUXQInPl6Er99dSLQlY",
-  authDomain: "curlbin-arsh.firebaseapp.com",
-  projectId: "curlbin-arsh",
-  storageBucket: "curlbin-arsh.appspot.com",
-  messagingSenderId: "906211009616",
-  appId: "1:906211009616:web:30b328df7b2df0b6fadd55",
-  measurementId: "G-8ZXJ2B1ZK1",
-};
+
 
 firebase.initializeApp(firebaseConfig);
 
